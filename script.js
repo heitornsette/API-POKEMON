21f3bbd8ea2cd52118494efdee4bb6cbd5928deb
Hns <51509684+heitornsette@users.noreply.github.com>
2026-02-12 17:17:06 -0300

    feat(pokedex): adiciona ordenação asc/desc e filtro por cores com atualização dinâmica do título

--- script.js
+++ script.js
@@ -21,6 +21,22 @@ const coresTipos = {
 
 let proximaUrl = null
 let AnteriorUrl = null
+let urlAtual = "https://pokeapi.co/api/v2/pokemon"
+let ordemAtual = "asc"
+
+function definirOrdem(ordem) {
+  ordemAtual = ordem
+  puxarPokemons(urlAtual)
+}
+
+async function buscarTipos(tipo) {
+  const request = await fetch(`https://pokeapi.co/api/v2/type/${tipo}/`)
+  const response = await request.json()
+
+  atualizarTitulo(`Type: ${response.name}`)
+
+  listarPokemons(response.pokemon, true)
+}
 
 async function puxarPokemons(
   url = "https://pokeapi.co/api/v2/pokemon",
@@ -29,11 +45,14 @@ async function puxarPokemons(
   const request = await fetch(url)
   const response = await request.json()
 
+  urlAtual = url
   proximaUrl = response.next
   AnteriorUrl = response.previous
-  const listaDePokemons = response.results
 
+  const listaDePokemons = response.results
   listarPokemons(listaDePokemons, false, buscarEspeciais)
+
+  atualizarContador(url, response.count, listaDePokemons.length)
 }
 
 async function listarPokemons(
@@ -57,8 +76,15 @@ async function listarPokemons(
   })
 
   const pokemonsDetalhados = await Promise.all(promessas)
+  const pokemonsValidos = pokemonsDetalhados.filter(Boolean)
 
-  pokemonsDetalhados.filter(Boolean).forEach(pokemon => {
+  if (ordemAtual === "desc") {
+    pokemonsValidos.sort((a, b) => b.id - a.id)
+  } else {
+    pokemonsValidos.sort((a, b) => a.id - b.id)
+  }
+
+  pokemonsValidos.forEach(pokemon => {
     renderizarCardPokemon(pokemon)
   })
 }
@@ -83,11 +109,17 @@ function renderizarCardPokemon(pokemon) {
 
   const imagemHtml = imagem
     ? `<img src="${imagem}" class="h-32 w-32 object-contain mx-auto my-2 scale-125">`
-    : `<div class="h-32 w-32 flex items-center justify-center mx-auto my-2 text-gray-400 text-xs"> Pokemon sem imagem </div>`
+    : `<div class="h-32 w-32 flex items-center justify-center mx-auto my-2 text-gray-400 text-xs"> Pokémon without image </div>`
 
   pokedex.innerHTML += `
-    <div class = " bg-white rounded-3xl p-6 shadow-sm hover:shadow-md  hover:scale-103 transition-all relative flex flex-col justify-between h-auto">
-      <p class="text-gray-400 font-bold text-sm">#${String(pokemon.id).padStart(4, "0")}</p>
+    <div class = " bg-white rounded-3xl py-6 px-4 shadow-sm hover:shadow-md hover:scale-102 transition-all relative flex flex-col justify-between h-auto cursor-pointer">
+      <div class= 'flex justify-between'>
+        <p class="text-gray-400 font-bold text-sm">#${String(pokemon.id).padStart(4, "0")}</p>
+        <button class="cursor-pointer" onclick="favoritarPokemon()">
+          <box-icon name='heart' color='#9ca3af' ></box-icon>
+        </button>
+        
+      </div>
       ${imagemHtml}
       <div>
         <p class = 'font-bold text-xl capitalize mb-2 text-gray-800'>${pokemon.name}</p>
@@ -99,12 +131,28 @@ function renderizarCardPokemon(pokemon) {
 
 puxarPokemons()
 
+function atualizarContador(url, total, quantidadeAtual) {
+  const contadorPokemon = document.querySelector("#contadorPokemons")
+
+  const urlObj = new URL(url)
+  const offSet = Number(urlObj.searchParams.get("offset")) || 0
+
+  const inicio = offSet + 1
+  const fim = offSet + quantidadeAtual
+
+  contadorPokemon.innerHTML = `Showing ${inicio}-${fim} of ${total} pokémons`
+}
+
+function atualizarTitulo(texto) {
+  const titulo = document.querySelector("#titulo")
+  titulo.innerHTML = texto
+}
+
 async function proximosPokemons() {
   if (proximaUrl) {
     await puxarPokemons(proximaUrl)
   }
 }
-
 async function anterioresPokemons() {
   if (AnteriorUrl) {
     await puxarPokemons(AnteriorUrl)
@@ -122,18 +170,37 @@ async function pesquisarPokemons(key, nome) {
   }
 }
 
-async function buscarTipos(tipo) {
-  const request = await fetch(`https://pokeapi.co/api/v2/type/${tipo}/`)
-  const response = await request.json()
-
-  listarPokemons(response.pokemon, true)
-}
-
 async function filtrarGeracao(inicio, fim, buscarEspeciais = false) {
+  atualizarTitulo(`Gen ${inicio} - ${fim}`)
   const limit = fim - inicio + 1
   const offset = inicio - 1
 
   const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
 
   await puxarPokemons(url, buscarEspeciais)
+}
+
+async function filtrarCor(cor) {
+  const request = await fetch(`https://pokeapi.co/api/v2/pokemon-color/${cor}/`)
+  const response = await request.json()
+
+  atualizarTitulo(`Color: ${response.name}`)
+
+  let specieList = response.pokemon_species
+
+  const pokemonUrlList = await Promise.all(
+    specieList.map(async species => {
+      const speciesRes = await fetch(species.url)
+      const speciesData = await speciesRes.json()
+
+      const def =
+        speciesData.varieties.find(v => v.is_default) ||
+        speciesData.varieties[0]
+      return {
+        url: def.pokemon.url,
+      }
+    }),
+  )
+
+  listarPokemons(pokemonUrlList)
 }

9005791ad6bf937d1154d962d2ecd1f769b495dd
Hns <51509684+heitornsette@users.noreply.github.com>
2026-02-11 23:49:42 -0300

    feat(js): implementar carregamento inicial de 20 pokémons, lógica de paginação e filtros dinâmicos

---
+++ script.js
@@ -0,0 +1,123 @@
+const coresTipos = {
+  grass: "var(--color-grass)",
+  poison: "var(--color-poison)",
+  fire: "var(--color-fire)",
+  water: "var(--color-water)",
+  bug: "var(--color-bug)",
+  normal: "var(--color-normal)",
+  electric: "var(--color-electric)",
+  ground: "var(--color-ground)",
+  fairy: "var(--color-fairy)",
+  fighting: "var(--color-fighting)",
+  psychic: "var(--color-psychic)",
+  rock: "var(--color-rock)",
+  ghost: "var(--color-ghost)",
+  ice: "var(--color-ice)",
+  dragon: "var(--color-dragon)",
+  steel: "var(--color-steel)",
+  dark: "var(--color-dark)",
+  flying: "var(--color-flying)",
+}
+
+let proximaUrl = null
+let AnteriorUrl = null
+
+async function puxarPokemons(url = "https://pokeapi.co/api/v2/pokemon") {
+  const request = await fetch(url)
+  const response = await request.json()
+
+  proximaUrl = response.next
+  AnteriorUrl = response.previous
+  const listaDePokemons = response.results
+
+  listarPokemons(listaDePokemons)
+}
+
+async function listarPokemons(arraysDePokemons, buscarPorTipo = false) {
+  let pokedex = document.querySelector("#pokedex")
+  pokedex.innerHTML = ""
+
+  const promessas = arraysDePokemons.map(async pokemon => {
+    let url = buscarPorTipo ? pokemon.pokemon.url : pokemon.url
+    const request = await fetch(url)
+    const response = await request.json()
+
+    if (response.id > 1025) {
+      return
+    }
+
+    return await response
+  })
+
+  const pokemonsDetalhados = await Promise.all(promessas)
+
+  pokemonsDetalhados.forEach(pokemon => {
+    renderizarCardPokemon(pokemon)
+  })
+}
+
+function renderizarCardPokemon(pokemon) {
+  let pokedex = document.querySelector("#pokedex")
+
+  let tiposHtml = ""
+  pokemon.types.forEach(item => {
+    const nomeTipo = item.type.name
+    const cor = coresTipos[nomeTipo]
+
+    tiposHtml += `
+        <p class="px-2 py-1 rounded-md text-white text-xs uppercase" 
+           style="background-color: ${cor}">
+           ${nomeTipo}
+        </p>`
+  })
+
+  pokedex.innerHTML += `
+    <div class = " bg-white rounded-3xl p-6 shadow-sm hover:shadow-md  hover:scale-103 transition-all relative flex flex-col justify-between h-auto">
+      <p class="text-gray-400 font-bold text-sm">#${String(pokemon.id).padStart(4, "0")}</p>
+      <img src="${pokemon.sprites.front_default}" class="h-32 w-32 object-contain mx-auto my-2 scale-125">
+      <div>
+        <p class = 'font-bold text-xl capitalize mb-2 text-gray-800'>${pokemon.name}</p>
+        <div class = 'flex gap-2 font-bold'>${tiposHtml}</div>
+      </div>
+    </div>
+    `
+}
+
+puxarPokemons()
+
+async function proximosPokemons() {
+  if (proximaUrl) {
+    await puxarPokemons(proximaUrl)
+  }
+}
+
+async function anterioresPokemons() {
+  if (AnteriorUrl) {
+    await puxarPokemons(AnteriorUrl)
+  }
+}
+
+async function pesquisarPokemons(key, nome) {
+  if (key === 13) {
+    const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`)
+    const response = await request.json()
+
+    listarPokemons(response)
+  }
+}
+
+async function buscarTipos(tipo) {
+  const request = await fetch(`https://pokeapi.co/api/v2/type/${tipo}/`)
+  const response = await request.json()
+
+  listarPokemons(response.pokemon, true)
+}
+
+async function filtrarGeracao(inicio, fim) {
+  const limit = fim - inicio + 1
+  const offset = inicio - 1
+
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+
+  await puxarPokemons(url)
+}
